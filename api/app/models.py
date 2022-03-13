310ceeb4dddb3e92551859ce40ed65be34f0652a
nalaminos <nalaminos@gmail.com>
2022-03-13 20:10:01 +0100

    Adding testing

--- api/app/models.py
+++ api/app/models.py
@@ -5,14 +5,14 @@ from pydantic import BaseModel #serializamos los json que entran y salen de la a
 class PredictionRequest(BaseModel):
     # Variables que utiliza el modelo para predecir
     opening_gross: float
-    screems: float
+    screens: float
     production_budget: float
     title_year: int
     aspect_ratio: float
     duration: int
     cast_total_facebook_likes: float
     budget: float
-    imb_score: float
+    imdb_score: float
 
 class PredictionResponse(BaseModel):
     # Variable a predecir
